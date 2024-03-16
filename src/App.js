4c933dfb700c797af621d9c2bdb077969cdbd23f
EswaraSatya <101344518+EswaraSatya@users.noreply.github.com>
2024-03-16 19:59:52 +0530

    latest changes
    Regarding build issue

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PokeCard } from "./Component/PokeDetails/PokeCard";
 import { DetailPage } from "./Component/PokeDetails/DetailPage";
-import React, { Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const PokeCard = React.lazy(() => import('./Component/PokeDetails/PokeCard'));
-const DetailPage = React.lazy(() => import('./Component/PokeDetails/DetailPage'));
+// const PokeCard = React.lazy(() => import('./Component/PokeDetails/PokeCard'));
+// const DetailPage = React.lazy(() => import('./Component/PokeDetails/DetailPage'));
 
 function App() {
 
@@ -14,10 +14,10 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Suspense fallback={<div>Loading...</div>}>
+          {/* <Suspense fallback={<div>Loading...</div>}> */}
             <Route path="/" element={<PokeCard />} />
             <Route path="/pokedetails/:id" element={<DetailPage />} />
-          </Suspense>
+          {/* </Suspense> */}
         </Routes>
       </Router>
     </div>
